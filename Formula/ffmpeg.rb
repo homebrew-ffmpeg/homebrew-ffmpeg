f979ba93bfbb4fff5f1eb7b61b84b66c2b2ae7a7
Chris Varenhorst <varenc@gmail.com>
2019-02-05 13:18:07 -0800

    init

---
+++ Formula/ffmpeg.rb
@@ -0,0 +1,172 @@
+class Ffmpeg < Formula
+  desc "Play, record, convert, and stream audio and video"
+  homepage "https://ffmpeg.org/"
+  url "https://ffmpeg.org/releases/ffmpeg-4.1.tar.xz"
+  version "4.1-with-options" # to distinguish from homebrew-core's ffmpeg
+  sha256 "a38ec4d026efb58506a99ad5cd23d5a9793b4bf415f2c4c2e9c1bb444acd1994"
+  head "https://github.com/FFmpeg/FFmpeg.git"
+
+### disable bottles since they might not match and this is for people that need to compile it themselves anyway
+#  bottle do
+#    rebuild 1
+#    sha256 "deeef583bec7a4f92a73846bfda57d8dc0afe6f039245389ca0668b3d76a88d2" => :mojave
+#    sha256 "fc62b7833925baf83d2792068b4dfe62c001d4515347375631efadcef727643a" => :high_sierra
+#    sha256 "71c3ef3e6cb313833ca19c735599416b8290e8b5b307242c64659367d05dc984" => :sierra
+#  end
+
+  option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
+  option "with-fdk-aac", "Enable the Fraunhofer FDK AAC library"
+  option "with-librsvg", "Enable SVG files as inputs via librsvg"
+  option "with-libsoxr", "Enable the soxr resample library"
+  option "with-libssh", "Enable SFTP protocol via libssh"
+  option "with-tesseract", "Enable the tesseract OCR engine"
+  option "with-libvidstab", "Enable vid.stab support for video stabilization"
+  option "with-openh264", "Enable OpenH264 library"
+  option "with-openjpeg", "Enable JPEG 2000 image format"
+  option "with-openssl", "Enable SSL support"
+  option "with-rubberband", "Enable rubberband library"
+  option "with-webp", "Enable using libwebp to encode WEBP images"
+  option "with-zeromq", "Enable using libzeromq to receive commands sent through a libzeromq client"
+  option "with-zimg", "Enable z.lib zimg library"
+  option "with-srt", "Enable SRT library"
+  option "with-libvmaf", "Enable libvmaf scoring library"
+
+  deprecated_option "with-libtesseract" => "with-tesseract"
+
+  depends_on "nasm" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texi2html" => :build
+
+  depends_on "aom"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "frei0r"
+  depends_on "lame"
+  depends_on "libass"
+  depends_on "libvorbis"
+  depends_on "libvpx"
+  depends_on "opencore-amr"
+  depends_on "opus"
+  depends_on "rtmpdump"
+  depends_on "sdl2"
+  depends_on "snappy"
+  depends_on "speex"
+  depends_on "theora"
+  depends_on "x264"
+  depends_on "x265"
+  depends_on "xvid"
+  depends_on "xz"
+
+  depends_on "chromaprint" => :optional
+  depends_on "fdk-aac" => :optional
+  depends_on "game-music-emu" => :optional
+  depends_on "libbluray" => :optional
+  depends_on "libbs2b" => :optional
+  depends_on "libcaca" => :optional
+  depends_on "libgsm" => :optional
+  depends_on "libmodplug" => :optional
+  depends_on "librsvg" => :optional
+  depends_on "libsoxr" => :optional
+  depends_on "libssh" => :optional
+  depends_on "libvidstab" => :optional
+  depends_on "libvmaf" => :optional
+  depends_on "openh264" => :optional
+  depends_on "openjpeg" => :optional
+  depends_on "openssl" => :optional
+  depends_on "rubberband" => :optional
+  depends_on "srt" => :optional
+  depends_on "tesseract" => :optional
+  depends_on "two-lame" => :optional
+  depends_on "wavpack" => :optional
+  depends_on "webp" => :optional
+  depends_on "zeromq" => :optional
+  depends_on "zimg" => :optional
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --enable-hardcoded-tables
+      --enable-avresample
+      --cc=#{ENV.cc}
+      --host-cflags=#{ENV.cflags}
+      --host-ldflags=#{ENV.ldflags}
+      --enable-ffplay
+      --enable-gpl
+      --enable-libaom
+      --enable-libmp3lame
+      --enable-libopus
+      --enable-libsnappy
+      --enable-libtheora
+      --enable-libvorbis
+      --enable-libvpx
+      --enable-libx264
+      --enable-libx265
+      --enable-libxvid
+      --enable-lzma
+      --enable-libfontconfig
+      --enable-libfreetype
+      --enable-frei0r
+      --enable-libass
+      --enable-libopencore-amrnb
+      --enable-libopencore-amrwb
+      --enable-librtmp
+      --enable-libspeex
+      --enable-videotoolbox
+      --disable-libjack
+      --disable-indev=jack
+    ]
+
+    args << "--disable-htmlpages" # doubtful anyone will look at this. The same info is accessible through the man pages.
+    args << "--enable-chromaprint" if build.with? "chromaprint"
+    args << "--enable-libbluray" if build.with? "libbluray"
+    args << "--enable-libbs2b" if build.with? "libbs2b"
+    args << "--enable-libcaca" if build.with? "libcaca"
+    args << "--enable-libfdk-aac" if build.with? "fdk-aac"
+    args << "--enable-libgme" if build.with? "game-music-emu"
+    args << "--enable-libgsm" if build.with? "libgsm"
+    args << "--enable-libmodplug" if build.with? "libmodplug"
+    args << "--enable-libopenh264" if build.with? "openh264"
+    args << "--enable-librsvg" if build.with? "librsvg"
+    args << "--enable-librubberband" if build.with? "rubberband"
+    args << "--enable-libsoxr" if build.with? "libsoxr"
+    args << "--enable-libsrt" if build.with? "srt"
+    args << "--enable-libssh" if build.with? "libssh"
+    args << "--enable-libtesseract" if build.with? "tesseract"
+    args << "--enable-libtwolame" if build.with? "two-lame"
+    args << "--enable-libvidstab" if build.with? "libvidstab"
+    args << "--enable-libvmaf" if build.with? "libvmaf"
+    args << "--enable-libwavpack" if build.with? "wavpack"
+    args << "--enable-libwebp" if build.with? "webp"
+    args << "--enable-libzimg" if build.with? "zimg"
+    args << "--enable-libzmq" if build.with? "zeromq"
+    args << "--enable-opencl" if MacOS.version > :lion
+    args << "--enable-openssl" if build.with? "openssl"
+
+    if build.with? "openjpeg"
+      args << "--enable-libopenjpeg"
+      args << "--disable-decoder=jpeg2000"
+      args << "--extra-cflags=" + `pkg-config --cflags libopenjp2`.chomp
+    end
+
+    # These librares are GPL-incompatible, and require ffmpeg be built with
+    # the "--enable-nonfree" flag, which produces unredistributable libraries
+    args << "--enable-nonfree" if build.with?("fdk-aac") || build.with?("openssl")
+
+    system "./configure", *args
+    system "make", "install"
+
+    # Build and install additional FFmpeg tools
+    system "make", "alltools"
+    bin.install Dir["tools/*"].select { |f| File.executable? f }
+  end
+
+  test do
+    # Create an example mp4 file
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
+    assert_predicate mp4out, :exist?
+  end
+end
---
+++ README.md
@@ -0,0 +1,1 @@
+# homebrew-ffmpeg-with-options
