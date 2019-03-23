816a89697dae3e0cb78b819357d97d3b5e326abc
Reto Kromer <retokromer@users.noreply.github.com>
2019-03-23 06:14:23 +0100

    update to FFmpeg 4.1.2

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -1,9 +1,9 @@
 class Ffmpeg < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-4.1.1.tar.xz"
+  url "https://ffmpeg.org/releases/ffmpeg-4.1.2.tar.xz"
   version "4.1.1-with-options" # to distinguish from homebrew-core's ffmpeg
-  sha256 "373749824dfd334d84e55dff406729edfd1606575ee44dd485d97d45ea4d2d86"
+  sha256 "b95f0ae44798ab1434155ac7f81f30a7e9760a02282e4b5898372c22a335347b"
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   # This formula is for people that will compile with their chosen options
