b79cd1950fcbbe31f87366a584a4d9208eb7b423
Reto Kromer <retokromer@users.noreply.github.com>
2020-10-11 07:23:28 +0200

    fix srt issue

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -5,7 +5,7 @@ class Ffmpeg < Formula
   version "4.3.1-with-options" # to distinguish from homebrew-core's ffmpeg
   sha256 "ad009240d46e307b4e03a213a0f49c11b650e445b1f8be0dda2a9212b34d2ffb"
   license "GPL-2.0-or-later"
-  revision 3
+  revision 4
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
@@ -85,6 +85,13 @@ class Ffmpeg < Formula
 
   uses_from_macos "bzip2"
   uses_from_macos "zlib"
+
+  # https://trac.ffmpeg.org/ticket/8760
+  # Remove in next release
+  patch do
+    url "https://github.com/FFmpeg/FFmpeg/commit/7c59e1b0f285cd7c7b35fcd71f49c5fd52cf9315.patch?full_index=1"
+    sha256 "1cbe1b68d70eadd49080a6e512a35f3e230de26b6e1b1c859d9119906417737f"
+  end
 
   def install
     args = %W[
