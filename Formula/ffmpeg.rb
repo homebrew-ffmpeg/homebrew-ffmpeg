7ee789eb45e6b98fbc68cd54a4fe5146b2397d78
רטו • Reto <retokromer@users.noreply.github.com>
2022-07-23 12:08:01 +0200

    delete not longer needed `libvmaf` modifications

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -190,15 +190,6 @@ class Ffmpeg < Formula
       args << "--enable-libopencore-amrwb"
     end
 
-    if build.with? "libvmaf"
-      # Replace hardcoded default VMAF model path
-      %w[doc/filters.texi libavfilter/vf_libvmaf.c].each do |f|
-        inreplace f, "/usr/local/share/model", HOMEBREW_PREFIX/"share/libvmaf/model"
-        # Since libvmaf v2.0.0, `.pkl` model files have been deprecated in favor of `.json` model files.
-        inreplace f, "vmaf_v0.6.1.pkl", "vmaf_v0.6.1.json"
-      end
-    end
-
     system "./configure", *args
     system "make", "install"
 
