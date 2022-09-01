17e0a60002de48abe3d7a4edb1513b8d6f34049e
רטו • Reto <retokromer@users.noreply.github.com>
2022-09-01 10:01:03 +0200

    upgrade to FFmpeg 5.1.1

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -1,12 +1,11 @@
 class Ffmpeg < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-5.1.tar.xz"
-  version "5.1-with-options" # to distinguish from homebrew-core's ffmpeg
-  sha256 "55eb6aab5ee235550fa54a33eaf8bf1b4ec66c01453182b12f6a993d75698b03"
+  url "https://ffmpeg.org/releases/ffmpeg-5.1.1.tar.xz"
+  version "5.1.1-with-options" # to distinguish from homebrew-core's ffmpeg
+  sha256 "95bf3ff8c496511e71e958fb249e663c8c9c3de583c5bebc0f5a9745abbc0435"
   license "GPL-2.0-or-later"
-  revision 2
-  head "https://github.com/FFmpeg/FFmpeg.git"
+  head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
 
   option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
   option "with-decklink", "Enable DeckLink support"
