1b636b3bd77213dec8dd19875a0f5ba6a5c38319
רטו/רעטאָ/רֵיטוֹ • Reto <retokromer@users.noreply.github.com>
2023-11-10 05:45:08 +0100

    FFmpeg 6.0.1

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -1,22 +1,11 @@
 class Ffmpeg < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  version "6.0-with-options" # to distinguish from homebrew-core's ffmpeg
+  url "https://ffmpeg.org/releases/ffmpeg-6.0.1.tar.xz"
+  version "6.0.1-with-options" # to distinguish from homebrew-core's ffmpeg
+  sha256 "9b16b8731d78e596b4be0d720428ca42df642bb2d78342881ff7f5bc29fc9623"
   license "GPL-2.0-or-later"
-  revision 4
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
-
-  stable do
-    url "https://ffmpeg.org/releases/ffmpeg-6.0.tar.xz"
-    sha256 "57be87c22d9b49c112b6d24bc67d42508660e6b718b3db89c44e47e289137082"
-
-    # Fix for binutils, remove with `stable` block on next release
-    # https://www.linuxquestions.org/questions/slackware-14/regression-on-current-with-ffmpeg-4175727691/
-    patch do
-      url "https://github.com/FFmpeg/FFmpeg/commit/effadce6c756247ea8bae32dc13bb3e6f464f0eb.patch?full_index=1"
-      sha256 "9800c708313da78d537b61cfb750762bb8ad006ca9335b1724dbbca5669f5b24"
-    end
-  end
 
   option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
   option "with-decklink", "Enable DeckLink support"
