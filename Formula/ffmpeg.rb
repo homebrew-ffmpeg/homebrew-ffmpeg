9bac9b3317d1080c835cb954b7ca566c7a97607a
Reto Kromer <retokromer@users.noreply.github.com>
2023-12-31 08:06:48 +0100

    update to 6.1.1

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -1,11 +1,10 @@
 class Ffmpeg < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-6.1.tar.xz"
-  version "6.1-with-options" # to distinguish from homebrew-core's ffmpeg
-  sha256 "488c76e57dd9b3bee901f71d5c95eaf1db4a5a31fe46a28654e837144207c270"
+  url "https://ffmpeg.org/releases/ffmpeg-6.1.1.tar.xz"
+  version "6.1.1-with-options" # to distinguish from homebrew-core's ffmpeg
+  sha256 "b0aee531e4a91fed6bc031445dfc3ad8eb36de1fcb0fa2acdb589ecc9f8dc9cb"
   license "GPL-2.0-or-later"
-  revision 3
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
 
   option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
