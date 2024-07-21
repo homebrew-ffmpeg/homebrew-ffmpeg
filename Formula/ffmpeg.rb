3dce722c9563c62e951adcbc19992e5e3bad9bbf
רטו\רעטאָ\רֵיטוֹ • Reto <retokromer@users.noreply.github.com>
2024-07-21 16:31:53 +0200

    add `libdrm` dependency for Linux (#166)

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -5,6 +5,7 @@ class Ffmpeg < Formula
   version "7.0.1-with-options" # to distinguish from homebrew-core's ffmpeg
   sha256 "bce9eeb0f17ef8982390b1f37711a61b4290dc8c2a0c1a37b5857e85bfb0e4ff"
   license "GPL-2.0-or-later"
+  revision 1
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
 
   option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
@@ -119,6 +120,7 @@ class Ffmpeg < Formula
 
   on_linux do
     depends_on "alsa-lib"
+    depends_on "libdrm"
     depends_on "libxext"
     depends_on "libxv"
   end
