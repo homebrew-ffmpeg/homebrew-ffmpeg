946f6dce2393a66c81fb10f296ec379f776490b6
bbgdzxng1 <17679234+bbgdzxng1@users.noreply.github.com>
2024-10-23 15:40:10 -0700

    Resolve DependencyOrder as identified by Github CI
    After submitting PR, Github CI claimed:
    
    > "FormulaAudit/DependencyOrder: dependency "libtensorflow" (line 105) should be put before dependency "svt-av1" (line 111)"
    
    This commit reorders the dependencies.

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -101,8 +101,8 @@ class Ffmpeg < Formula
   depends_on "rubberband" => :optional
   depends_on "speex" => :optional
   depends_on "srt" => :optional
-  depends_on "svt-av1" => :optional
   depends_on "libtensorflow" => :optional
+  depends_on "svt-av1" => :optional
   depends_on "tesseract" => :optional
   depends_on "two-lame" => :optional
   depends_on "webp" => :optional
