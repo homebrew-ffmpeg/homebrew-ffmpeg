12e80facb5376c6b3aaebc11e341bd609ff4abca
רטו\רעטאָ\רֵיטוֹ • Reto <retokromer@users.noreply.github.com>
2026-03-17 12:33:25 +0100

    FFmpeg 8.1

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -2,18 +2,9 @@ class Ffmpeg < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
   license "GPL-2.0-or-later"
+  url "https://ffmpeg.org/releases/ffmpeg-8.1.tar.xz"
+  sha256 "b072aed6871998cce9b36e7774033105ca29e33632be5b6347f3206898e0756a"
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
-
-  stable do
-    url "https://ffmpeg.org/releases/ffmpeg-8.0.1.tar.xz"
-    sha256 "05ee0b03119b45c0bdb4df654b96802e909e0a752f72e4fe3794f487229e5a41"
-
-    # Add svt-av1 4.x support
-    patch do
-      url "https://git.ffmpeg.org/gitweb/ffmpeg.git/patch/a5d4c398b411a00ac09d8fe3b66117222323844c"
-      sha256 "1dbbc1a4cf9834b3902236abc27fefe982da03a14bcaa89fb90c7c8bd10a1664"
-    end
-  end
 
   option "with-alt-name", "Use command names ff*-alt rather than ff*"
   option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
