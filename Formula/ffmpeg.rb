c2929c94bdb67e43fad5b3f09053310f963c3ca2
slhck <582444+slhck@users.noreply.github.com>
2026-09-18 14:08:29 +0000

    Update FFmpeg to 9.0.2

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -1,8 +1,8 @@
 class Ffmpeg < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-9.0.1.tar.xz"
-  sha256 "cf38e0e28c7e5605942c4a77755349b0145804a397af37eb1fb4c77cb237f635"
+  url "https://ffmpeg.org/releases/ffmpeg-9.0.2.tar.xz"
+  sha256 "8c3850283eb25fa026482078a04051e0be17347b09ef81a0849bec15a96e002e"
   license "GPL-2.0-or-later"
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
 
